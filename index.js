27ecde5d47cb7141b99ab259a3ec63445075b2d6
Mr.Sadykov <Sadykov.Ramzan@gmail.com>
2022-08-16 19:02:19 +0300

    fdshg

---
+++ index.js
@@ -0,0 +1,34 @@
+// // console.log("Аджара гуджу, фитююю")
+// // console.log(2*3-1+100/4)
+// // console.log("yandex"+2)
+// // console.log(2**3)
+// // console.log((2+3)+(10-2)+(2*3)+(10/5)+(2**3))
+// // let n1 = 32
+// // console.log(n1)
+
+// // Типы данных
+
+// let nb1 = 1
+// let nb2 = 2
+// let nb3 = 3
+// let nb4 = 4
+// let nb5 = 5
+
+// let str1 = "one"
+// let str2 = "two"
+// let str3 = "free"
+// let str4 = "four"
+// let str5 = "five"
+// console.log(str1+nb1+nb2)
+// nb1 = 11
+// nb2 = 22
+// nb3 = 33
+
+// str1 = "один"
+// str2 = "два"
+// str3 = "три"
+// console.log(str1)
+
+let names = ["Razman", "Umar", "Deni", "Adlan", "Abubakar"];
+let age = [29, 100, 1000, 27,29];
+console.log ("Имя:", names[1],",", "Возраст", age[2]);
